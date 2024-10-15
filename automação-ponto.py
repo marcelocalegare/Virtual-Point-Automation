4fd1863a99edd543929f3ee8c98b61968372949b
marcelocalegare <marcelocalegare06@gmail.com>
2024-10-15 17:11:10 -0300

    Atualização de alguns erros do codigo na parte de verificação de horario.

--- automação-ponto.py
+++ automação-ponto.py
@@ -2,27 +2,29 @@ import pyautogui as pgui
 from time import sleep
 from datetime import datetime, time
 
-current_time = datetime.now().time() # Pega o horario atual e armazena em uma variavel
+break_time = time(17, 1) # Define um horario e armazena
 
-break_time = time(15, 30, 30) # Define um horario e armazena
+current_time = datetime.now().time().replace(second=0, microsecond=0) # Pega o horario atual e armazena em uma variavel
 
 while True:
-    pgui.hotkey('win','m') # Atalho até a area de trabalho
-    sleep(2) # Pausa de 5 segundos
-
 
     if current_time == break_time: # Faz a verificação se o horario atual esta igual ao horario do break time
-        confirmation = pgui.confirm(title='AUTOMAÇÃO PONTO VIRTUAL', text='DESEJA BATER O PONTO VIRTUAL AGORA?', buttons=['SIM','NÃO', 'CANCELAR']) # Recebe uma confirmação do usuario (sim, não, cancelar)
+        confirm = pgui.confirm(title='AUTOMAÇÃO PONTO VIRTUAL', text='DESEJA BATER O PONTO VIRTUAL AGORA?', buttons=['Sim','Não', 'Cancelar']) # Recebe uma confirmação do usuario (sim, não, cancelar)
 
-    if confirmation == 'NÃO': # Caso o usuario escolha a opção 'não' o programa pergunta daqui quanto tempo o usuario deseja realizar a automação e armazena em uma variavel
-        temp_minutos = pgui.prompt(title='AUTOMAÇÃO PONTO VIRTUAL', text='DAQUI QUANTO TEMPO DESEJA BATER SEU PONTO? [EM MINUTOS]') 
+        if confirm == 'Não': # Caso o usuario escolha a opção 'não' o programa pergunta daqui quanto tempo o usuario deseja realizar a automação e armazena em uma variavel
+            temp_minutos = int(pgui.prompt(title='AUTOMAÇÃO PONTO VIRTUAL', text='DAQUI QUANTO TEMPO DESEJA BATER SEU PONTO? [EM MINUTOS]'))
 
-    elif confirmation == 'CANCELAR':
-        break 
+        elif confirm == 'Cancelar':
+            break 
+
+    
 
     temp_segundos = temp_minutos * 60 # Transforma o tempo de minutos para segundos
 
     sleep(temp_segundos) # Pausa durante quanto tempo o usuario determinar
+
+    pgui.hotkey('win','m') # Atalho até a area de trabalho
+    sleep(2) # Pausa de 5 segundos
 
     pgui.moveTo(554,35,duration=2) # Move o cursor até o icone do ponto virtual
     sleep(1) # Pausa durante 1 segundo
